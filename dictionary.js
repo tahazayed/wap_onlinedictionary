d01d97b48956cc94ae5cca2f8b9c94e7ac9e29b4
Taha Elsayed <tahazayed@gmail.com>
2023-05-24 13:21:24 -0500

    cleanup

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const path = require('path');
 
 const app = express();
 
@@ -17,10 +18,10 @@ app.use(express.json());
 // for parsing application/xwww-
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function (req, res) {
-    return res.render("index_view");
+  return res.render("index_view");
 });
 
 app.get('/dict.html', function (req, res) {
