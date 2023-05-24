93c784ca5b90fa005f053e15caaccaffbabd3175
Taha Elsayed <tahazayed@gmail.com>
2023-05-24 12:49:42 -0500

    cleanup

--- dictionary.js
+++ dictionary.js
@@ -12,10 +12,10 @@ app.set('views', './views');
 
 
 // for parsing application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 // for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
@@ -26,29 +26,7 @@ app.get('/', function (req, res) {
 app.get('/dict.html', function (req, res) {
   return res.render("index_view");
 });
-/*
-// Define the GET API route for searching a word in the dictionary
-app.get('/api/dictionary', (req, res) => {
-  const searchWord = req.query.word;
 
-  // Perform the database query to search for the word
-  const query = `SELECT * FROM entries WHERE word = ?`;
-  connection.query(query, [searchWord], (error, results) => {
-    if (error) {
-      console.error('Error executing the query:', error);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
-    // Handle the query results
-    if (results.length === 0) {
-      res.status(404).send('Word not found');
-    } else {
-      res.json(results[0]);
-    }
-  });
-});
-*/
 app.use('/api/word', word);
 // Start the server
 const port = 3000;
